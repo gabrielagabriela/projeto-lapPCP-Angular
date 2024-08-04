506f0677566d13141606d6d2efdb35b64b3de0a0
gabrielagabriela <gabrielaa.slv@gmail.com>
2024-08-04 14:47:47 -0300

    criação componente menu-lateral

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
+import { MenuLateralComponent } from './shared/components/menuLateral/menu-lateral/menu-lateral.component';
 
 
 export const routes: Routes = [
