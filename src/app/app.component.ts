5f0e6d116df35553b67d5127b0fc84bed1fca12a
gabrielagabriela <gabrielaa.slv@gmail.com>
2024-08-04 12:38:18 -0300

    criação toolbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,25 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
+import { ToolbarComponent } from './shared/components/toolbar/toolbar.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ToolbarComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'labpcp-angular';
+
+  showToolbar = true;
+
+  constructor(private router: Router){
+    this.router.events.subscribe(retorno => {
+      if(retorno instanceof NavigationEnd){
+        this.showToolbar = !this.router.url.includes('login')
+      }
+    })
+  }
 }
