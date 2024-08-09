037672fc2fe4557ece9179f8cd71e3bddd727b25
gabrielagabriela <gabrielaa.slv@gmail.com>
2024-08-09 12:00:53 -0300

    componente, tamplate e tratamento de listagem de docentes

--- src/app/pages/cadastroDocente/cadastro-docente/cadastro-docente.component.ts
+++ src/app/pages/cadastroDocente/cadastro-docente/cadastro-docente.component.ts
@@ -3,25 +3,37 @@ import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { DocenteInterface } from '../../../shared/interfaces/docente.interface';
 import { DocenteService } from '../../../core/services/docente/docente.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-cadastro-docente',
   standalone: true,
   imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './cadastro-docente.component.html',
-  styleUrl: './cadastro-docente.component.scss'
+  styleUrl: './cadastro-docente.component.scss',
 })
 export class CadastroDocenteComponent implements OnInit {
-
   cadastroForm!: FormGroup;
+  id!: string;
 
-  constructor(private docenteService: DocenteService ){}
+  constructor(
+    private docenteService: DocenteService,
+    public activatedRoute: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
-      this.criarForm();
+    this.criarForm();
+    this.id = this.activatedRoute.snapshot.params['id'];
+    if (this.id) {
+      this.docenteService.getDocenteById(this.id).subscribe((retorno) => {
+        if (retorno) {
+          this.cadastroForm.patchValue(retorno);
+        }
+      });
+    }
   }
 
-  criarForm(){
+  criarForm() {
     this.cadastroForm = new FormGroup({
       nome: new FormControl(''),
       perfil: new FormControl('docente'),
@@ -35,21 +47,35 @@ export class CadastroDocenteComponent implements OnInit {
       rg: new FormControl(''),
       naturalidade: new FormControl(''),
       materias: new FormControl(''),
-      endereco: new FormControl
-    })
+      endereco: new FormControl(),
+    });
   }
 
-  onSubmit(){
-    if(this.cadastroForm.valid){
-      this.cadastrar(this.cadastroForm.value);
+  onSubmit() {
+    if (this.cadastroForm.valid) {
+      if (this.id) {
+        this.editar(this.cadastroForm.value);
+      } else {
+        this.cadastrar(this.cadastroForm.value);
+      }
+    } else {
+      alert('Preencha os campos');
     }
   }
 
-  cadastrar(usuario: DocenteInterface){
-    this.docenteService.postDocente(this.cadastroForm.value).subscribe((retorno) => {
-      console.log(retorno)
-      window.alert("Docente cadastrado com sucesso!");
-    })
+  cadastrar(usuario: DocenteInterface) {
+    this.docenteService
+      .postDocente(this.cadastroForm.value)
+      .subscribe((retorno) => {
+        console.log(retorno);
+        window.alert('Docente cadastrado com sucesso!');
+      });
+  }
+
+  editar(usuario: DocenteInterface) {
+    usuario.id = this.id;
+    this.docenteService.putDocente(usuario).subscribe((retorno) => {
+      window.alert('Docente editado com sucesso!');
+    });
   }
 }
-
